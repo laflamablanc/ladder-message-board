e7434d80d1fee6de88ac596adb5a99e6fc7db6f9
Sean LaFlam <sean.laflam@gmail.com>
2021-03-11 16:05:42 -0500

    Submit

--- src/components/MessageBar.js
+++ src/components/MessageBar.js
@@ -1,12 +1,35 @@
 import React from 'react'
 
-function MessageBar(){
-    return(
-        <div className = "MessageBar">
-            <input placeholder="Write you post here..."></input>
-            <button>Send</button>
-        </div>
-    )
+class MessageBar extends React.Component {
+
+    state = {
+        messageText: ""
+    }
+
+    inputHandler = (e) => {
+        this.setState({messageText: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit')
+        this.setState({messageText:""})
+    }
+
+    render(){
+        console.log(this.state.messageText)
+        return(
+            <form className = "MessageBar" onSubmit={this.submitHandler}>
+                <input
+                    type = "text" 
+                    placeholder="Write you post here..." 
+                    onChange={this.inputHandler} 
+                    value= {this.state.messageText}
+                />
+                <input id = "button" type="submit" value="Send"/>
+            </form>
+        )
+    }
 }
 
 export default MessageBar
