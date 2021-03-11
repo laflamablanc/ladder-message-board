5d0bce6eed26ff22a61da5fd755b2251f7753e92
Sean LaFlam <sean.laflam@gmail.com>
2021-03-11 00:39:20 -0500

    MB & M

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message(props){
+    const { text, uid } = props.Message
+    
+    return <p>text</p>
+
+}
+
+
+export default Message
--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -6,35 +6,23 @@ function MessageBoard(){
 
     const [messages,setMessages]=useState([])
 
-    const fetchBlogs=async()=>{
-        const response=db.collection('messages');
-        const data=await response.get();
-        data.docs.forEach(item=>{
-         setBlogs([...blogs,item.data()])
+    const fetchBlogs = async() => {
+        const response = db.collection('messages');
+        const data = await response.get();
+        data.docs.forEach(item => {
+            setMessages([...messages,item.data()])
         })
     }
 
+    useEffect(() => {
+        fetchBlogs();
+    }, [])
     
-    // var query = db.collection('messages').orderBy('timestamp').limit(100)
-
-    // query.onSnapshot(function(snapshot) {
-    //     snapshot.docChanges().forEach(function(change) {
-    //       if (change.type === 'removed') {
-    //         deleteMessage(change.doc.id);
-    //       } else {
-    //         var message = change.doc.data();
-    //         displayMessage(change.doc.id, message.timestamp, message.name,
-    //                        message.text, message.profilePicUrl, message.imageUrl);
-    //       }
-    //     });
-    //   });
-
-    // console.log("query:", query)
 
 
     return(
         <div>
-            MessageBoard
+            { messages && messages.map(msg => <Message/>)}
         </div>
     )
 
